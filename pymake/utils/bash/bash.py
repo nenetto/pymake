87d10b618103a3ca3fd6e9a57f997ef77b6a5c6d
nenetto <nenetto@gmail.com>
2018-05-15 11:15:50 +0200

    fix error for subprocess no wait

--- pymake/utils/bash/bash.py
+++ pymake/utils/bash/bash.py
@@ -18,17 +18,17 @@ def run_shell_command(command, wait=True):
         stdout, stderr = session.communicate()
         session.wait()
 
-    output_lines = stdout.decode("utf-8").split('\n')
-    for ol in output_lines:
-        pm.print_info_2(ol, padding=3)
+        output_lines = stdout.decode("utf-8").split('\n')
+        for ol in output_lines:
+            pm.print_info_2(ol, padding=3)
 
-    if session.returncode != 0:
-        e = stderr.decode("utf-8")
-        pm.print_error('Installation failed!')
-        pm.print_info_2('Error: {0}'.format(str(e)))
-        pm.print_error('Exit', exit_code=0)
+        if session.returncode != 0:
+            e = stderr.decode("utf-8")
+            pm.print_error('Installation failed!')
+            pm.print_info_2('Error: {0}'.format(str(e)))
+            pm.print_error('Exit', exit_code=0)
 
-    else:
-        e = stderr.decode("utf-8").split('\n')
-        for ee in e:
-            pm.print_info(ee, padding=4)
+        else:
+            e = stderr.decode("utf-8").split('\n')
+            for ee in e:
+                pm.print_info(ee, padding=4)
