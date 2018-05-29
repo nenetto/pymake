955949dd2a6751d5d059f95cef103968250825b6
beeva-rafaelalvarez <31099343+beeva-rafaelalvarez@users.noreply.github.com>
2018-05-29 10:45:02 +0200

    Added waiter to long time query execution

--- pymake/utils/aws/athena.py
+++ pymake/utils/aws/athena.py
@@ -97,7 +97,7 @@ def reload_partitions_in_table(athena_database, athena_table, s3_bucketname, fil
     output_location = 's3://' + '/'.join([s3_bucketname, file_remote_path]) + '/'
 
     try:
-        _ = athena.start_query_execution(QueryString='MSCK REPAIR TABLE {0};'.format(athena_table),
+        response = athena.start_query_execution(QueryString='MSCK REPAIR TABLE {0};'.format(athena_table),
                                                 QueryExecutionContext={'Database': athena_database},
                                                 ResultConfiguration={'OutputLocation': output_location,
                                                                      'EncryptionConfiguration': {
@@ -106,6 +106,28 @@ def reload_partitions_in_table(athena_database, athena_table, s3_bucketname, fil
                                                 )
     except ClientError as err:
         pm.print_error('Reload partitions failed on table [{0}.{1}]'.format(athena_database, athena_table))
+        pm.print_error(err.response['Error']['Message'], exit_code=1)
+    try:
+
+        while True:
+            status = athena.get_query_execution(QueryExecutionId=response['QueryExecutionId'])
+            current_status = status['QueryExecution']['Status']['State']
+
+            if current_status not in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+                if verbose:
+                    pm.print_info_flush(msg='Query Status: {0}'.format(current_status), wait=True)
+            elif current_status == 'SUCCEEDED':
+                if verbose:
+                    pm.print_info_flush(msg='Query Status: {0}'.format(current_status), wait=False)
+                query_result = athena.get_query_results(QueryExecutionId=response['QueryExecutionId'])
+                break
+            else:
+                if verbose:
+                    pm.print_error('Query {0}'.format(current_status))
+                return None
+
+    except ClientError as err:
+        pm.print_warning('Athena [{0}] error'.format(athena_database))
         pm.print_error(err.response['Error']['Message'], exit_code=1)
     else:
         pm.print_info('Reload partitions succeed on table [{0}.{1}]'.format(athena_database, athena_table))
