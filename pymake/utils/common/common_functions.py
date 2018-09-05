597926c789d7b3b307fc0eaa4ccaea609b5b708d
nenetto <nenetto@gmail.com>
2018-09-05 09:50:53 +0200

    Add new support for configuration json files

--- pymake/utils/common/common_functions.py
+++ pymake/utils/common/common_functions.py
@@ -131,35 +131,48 @@ def read_env_var(name):
         pm.print_error('Environment variable [{0}] not found'.format(name), exit_code=1)
     return None
 
-def load_env_var_from_dict(envar_dict, update=True):
+
+def load_env_var_from_dict(envar_dict, prefix='', update=True):
+
+    newprefix = prefix + '_' if prefix != '' else ''
 
     for k, v in envar_dict.items():
         if isinstance(v, dict):
-            load_env_var_from_dict(v)
+            load_env_var_from_dict(v, newprefix + k)
         else:
-            if update and k in os.environ:
-                pm.print_warning('Updating environment variable [{0}]:[{1}]->[{2}]'.format(k, os.environ[k], v))
-                os.environ[k] = v
-            elif k not in os.environ:
-                pm.print_warning('Setting environment variable [{0}]:[{1}]'.format(k, v))
-                os.environ[k] = v
+            varname = newprefix + k
+
+            if update and varname in os.environ:
+                pm.print_warning('Updating environment variable [{0}]:[{1}]->[{2}]'.format(varname,
+                                                                                           os.environ[varname],
+                                                                                           v))
+                os.environ[varname] = v
+            elif varname not in os.environ:
+                pm.print_info('Setting environment variable [{0}]:[{1}]'.format(varname, v))
+                os.environ[varname] = v
             elif k in os.environ:
-                pm.print_warning('Found enviroment variable [{0}]:[{1}]- no replaced by new value [{2}]'.format(k, os.environ[k], v))
+                pm.print_warning('Found enviroment variable [{0}]:[{1}]- no replaced by new value [{2}]'.format(varname,
+                                                                                                                os.environ[varname],
+                                                                                                                v))
 
 
 def load_env_variables(configuration_file, update=True):
     df = json2dict(configuration_file)
-    load_env_var_from_dict(df, update)
+    load_env_var_from_dict(df, prefix='', update=update)
 
 
-def unload_env_var_from_dict(envar_dict):
+def unload_env_var_from_dict(envar_dict, prefix=''):
+
+    newprefix = prefix + '_' if prefix != '' else ''
 
     for k, v in envar_dict.items():
         if isinstance(v, dict):
-            unload_env_var_from_dict(v)
+            unload_env_var_from_dict(v, newprefix + k)
         else:
-            if k in os.environ:
-                del os.environ[k]
+            varname = newprefix + k
+            if varname in os.environ:
+                del os.environ[varname]
+
 
 def unload_env_variables(configuration_file):
     df = json2dict(configuration_file)
