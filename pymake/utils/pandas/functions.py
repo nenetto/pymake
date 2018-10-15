576d0d5b4ea766634acc0bf0c2b24a7a77d85e35
nenetto <nenetto@gmail.com>
2018-10-15 09:23:22 +0200

    XLS function

--- pymake/utils/pandas/functions.py
+++ pymake/utils/pandas/functions.py
@@ -6,12 +6,14 @@ pymake
 -------------------------------
 Created 29-05-2018
 """
+from __future__ import absolute_import
 import pandas
 from pymake.main import printer
 import re
 import unidecode
 import random
-import numpy as np
+from openpyxl import load_workbook
+import os
 
 
 def normalize_str(instr):
@@ -55,6 +57,103 @@ def read_csv_random_sample(filename, nsamples):
     df = pandas.read_csv(filename, skiprows=skip)
 
     return df
+
+
+def lxml_available():
+    try:
+        from lxml.etree import LXML_VERSION
+        LXML = LXML_VERSION >= (3, 3, 1, 0)
+        if not LXML:
+            import warnings
+            warnings.warn("The installed version of lxml is too old to be used with openpyxl")
+            return False  # we have it, but too old
+        else:
+            return True  # we have it, and recent enough
+    except ImportError:
+        return False  # we don't even have it
+
+
+def lxml_env_set():
+    return os.environ.get("OPENPYXL_LXML", "True") == "True"
+
+
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False,
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+    """
+    LXML = lxml_available() and lxml_env_set()
+
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pandas.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename,
+                                    read_only=False,
+                                    keep_vba=False,
+                                    data_only=True,
+                                    guess_types=False,
+                                    keep_links=False)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
+
 
 if __name__ == "__main__":
     data = []
