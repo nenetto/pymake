f96cdc69d9d9065c265a84dcc3efc5ee4bfb5aef
nenetto <nenetto@gmail.com>
2019-08-08 12:34:44 +0200

    Generate python egg

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
         name='pymake',
-        version='1.0.1',
+        version='1.0.2',
         description='',
         long_description=long_description,
         url='https://github.com/nenetto/pymake',
